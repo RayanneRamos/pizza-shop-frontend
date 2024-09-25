3b351c8982577b9cad06e35efc044f503b0ba7b2
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-25 11:22:18 -0300

    usando react hook form

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -7,7 +7,7 @@ export function AuthLayout() {
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
-          <span className="font-semibold">pizza.shop</span>
+          <span className="font-bold">pizza.shop</span>
         </div>
         <footer className="text-sm">
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
